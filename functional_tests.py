b6f9b20ebe008755dc4993ffc36daf7bb90795bf
stephenl6705 <stephen.langestraat@gmail.com>
2016-02-26 05:19:04 +0000

    Functional test now checks we can link to an rstudio and shiny apps page

--- functional_tests.py
+++ functional_tests.py
@@ -17,15 +17,27 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices the page title and header mention innovation platform
         self.assertIn('Innovation Platform', self.browser.title)
-        self.fail('Finish the test')
 
         # He is invited to go to our RStudio site and to our Shiny Applications site
+        rstudio = self.browser.find_element_by_id('id_rstudio')
+        rstudio_link = rstudio.find_element_by_tag_name('a')
+        self.assertEqual(rstudio_link.text,"RStudio","The link was:\n%s" % (rstudio_link.text,))
+        shinyapps = self.browser.find_element_by_id('id_shinyapps')
+        shinyapps_link = shinyapps.find_element_by_tag_name('a')
+        self.assertEqual(shinyapps_link.text,"Shiny Apps","The link was:\n%s" % (shinyapps_link.text,))
 
         # He selects the RStudio link and now find himself on the Rstudio site
+        rstudio_link.click()
+        self.assertIn('RStudio Sign In', self.browser.title)
 
         # He goes back to the innovation home site and selects the shiny applications site
+        self.browser.get('http://innovation.seanapsys.com')
+        shinyapps_link.click()
+        self.assertIn('Shiny Apps', self.browser.title)
+
         # He now finds himself on a page showing a list of Shiny Applications
 
+        self.fail('Finish the test')
 
 
 if __name__ == '__main__':
