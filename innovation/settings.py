278c5482601dd6084091521d4df989e2506086a2
stephenl6705 <stephen.langestraat@gmail.com>
2016-03-02 04:54:47 +0000

    include README.md

--- innovation/settings.py
+++ innovation/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'innovation.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
