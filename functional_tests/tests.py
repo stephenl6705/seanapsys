2b021e1cdb9f06d7bb0a9fd42754c9baac175a22
stephenl6705 <stephen.langestraat@gmail.com>
2016-03-07 02:07:05 +0000

    create separate defs for tests in FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
+from unittest import skip
 import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
-waittime = 0
+waittime = 3
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
@@ -27,6 +28,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    @skip
     def test_can_link_to_r_studio_site(self):
 
         # Isaac has heard about a cool new innovation platform. He goes and checks out the homepage
@@ -45,6 +47,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser.implicitly_wait(waittime)
         self.assertIn('RStudio Sign In', self.browser.title)
 
+    @skip
+    def test_can_link_to_shiny_site(self):
+
         # He goes back to the innovation home site and selects the shiny applications site
         self.browser.get(self.server_url)
         shinyapps = self.browser.find_element_by_id('id_shinyapps')
@@ -52,17 +57,28 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertEqual(shinyapps_link.text,"Shiny Apps","The link was:\n%s" % (shinyapps_link.text,))
         shinyapps_link.click()
         self.browser.implicitly_wait(waittime)
+        # He now finds himself on a page showing a list of Shiny Applications
         self.assertIn('Shiny Apps', self.browser.title)
 
-        # He now finds himself on a page showing a list of Shiny Applications
+    def test_can_link_to_shinyapp1_site(self):
+
+        # Isaac is now also curious about the first app that he saw
+        self.browser.get(self.server_url)
+        shinyapps = self.browser.find_element_by_id('id_shinyapps')
+        shinyapps_link = shinyapps.find_element_by_tag_name('a')
+        shinyapps_link.click()
+        #self.browser.implicitly_wait(waittime)
         shinyapp1 = self.browser.find_element_by_id('id_shinyapp1')
         shinyapp1_link = shinyapp1.find_element_by_tag_name('a')
         self.assertEqual(shinyapp1_link.text,"Hello App","The link was:\n%s" % (shinyapp1_link.text,))
         shinyapp1_link.click()
         self.browser.implicitly_wait(waittime)
-        self.assertIn("It's Alive!", self.browser.title)
+        self.assertIn("Alive", self.browser.title)
 
-        # Isaac is now also curious about the other app that he saw
+    @skip
+    def test_can_link_to_shinyapp2_site(self):
+
+        # Isaac is now also curious about the second app that he saw
         self.browser.get(self.server_url)
         shinyapps = self.browser.find_element_by_id('id_shinyapps')
         shinyapps_link = shinyapps.find_element_by_tag_name('a')
